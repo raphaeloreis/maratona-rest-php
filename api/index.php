cc698e2a9871b19723173ca9576194d6f547817a
raphaeloreis <raphaelodreis@gmail.com>
2021-02-16 22:48:36 -0300

    Ajustes de formatação

--- api/index.php
+++ api/index.php
@@ -3,110 +3,89 @@ header('Content-Type: application/json; charset=utf-8');
 
 class Rest
 {
-	public static function open($requisicao)
-	{
-		$url = explode('/', $requisicao['url']);
-		
-		$classe = ucfirst($url[0]);
-		array_shift($url);
+    public static function open($requisicao)
+    {
+        $url = explode('/', $requisicao['url']);
 
-		$parametros = array();
-		$parametros = $url;
-		$erro = array();
+        $classe = ucfirst($url[0]);
+        array_shift($url);
 
-		require_once 'model/'.$classe.'.php';
+        $parametros = array();
+        $parametros = $url;
+        $erro = array();
 
-		$tipo_requisicao = $_SERVER['REQUEST_METHOD'];
+        require_once 'model/' . $classe . '.php';
 
-		if($tipo_requisicao=='GET'){
-			if($classe == 'Listagem'){
-				$retorno = call_user_func_array(array(new $classe, $parametros[0]), array());
-			}else{
-				@$id = $parametros[0];
-				if($id>0){
-					$retorno = call_user_func_array(array(new $classe, 'find'), $parametros);
-				}else{
-					$retorno = call_user_func_array(array(new $classe, 'findAll'),$parametros);
-				}
-	
-				if(!$retorno){
-					$erro['status'] = 'erro';
-					$erro['dados'] = "Registro $id do objeto $classe não localizado";
-				}
-			}
-		}elseif ($tipo_requisicao=='POST') {
-			
-			$json = file_get_contents('php://input');
-			$post = json_decode($json);
+        $tipo_requisicao = $_SERVER['REQUEST_METHOD'];
 
-			$erro = call_user_func_array(array(new $classe, 'validaDado'), array($post));
-			if(!$erro){
-				$retorno = call_user_func_array(array(new $classe, 'insert'), array($post));
-			}
+        if ($tipo_requisicao == 'GET') {
+            if ($classe == 'Listagem') {
+                $retorno = call_user_func_array(array(new $classe, $parametros[0]), array());
+            } else {
+                @$id = $parametros[0];
+                if ($id > 0) {
+                    $retorno = call_user_func_array(array(new $classe, 'find'), $parametros);
+                } else {
+                    $retorno = call_user_func_array(array(new $classe, 'findAll'), $parametros);
+                }
 
-		}elseif($tipo_requisicao=='PUT'){
-			$json = file_get_contents('php://input');
-			$post = json_decode($json);
-			
-			if($parametros[0]>0){
-				$id = $parametros[0];
+                if (!$retorno) {
+                    $erro['status'] = 'erro';
+                    $erro['dados'] = "Registro $id do objeto $classe não localizado";
+                }
+            }
+        } elseif ($tipo_requisicao == 'POST') {
 
-				$erro = call_user_func_array(array(new $classe, 'validaDado'), array($post));
-				if(!$erro){
-					$registro = call_user_func_array(array(new $classe, 'find'), $parametros);
-					if(empty($registro)) { 
-						$erro['status'] = 'erro';
-						$erro['dados'] = "Registro $id do objeto $classe não localizado";
-					}else{
-						
-						$retorno = call_user_func_array(array(new $classe, 'update'), array($post,$id));
-						
-					}
-				}
-			}
-		}elseif($tipo_requisicao=='DELETE'){
-			
-			@$id = $parametros[0];
-			if($id>0){
-				$registro = call_user_func_array(array(new $classe, 'find'), $parametros);
-				if(empty($registro)) { 
-					$erro['status'] = 'erro';
-					$erro['dados'] = "Registro $id do objeto $classe não localizado";	
-				}else{
-					$retorno = call_user_func_array(array(new $classe, 'delete'), array($id));
-					
-				}
-			}
-		}
-		if($erro){
-			return json_encode($erro);
-		}else{
-			return json_encode(array('status' => 'sucesso', 'dados' => $retorno));
-		}
-		exit;
-		
-		try {
-			if (class_exists($classe)) {
-				
-				
-				
-				if (method_exists($classe, $metodo)) {
-					$retorno = call_user_func_array(array(new $classe, $metodo), $parametros);
+            $json = file_get_contents('php://input');
+            $post = json_decode($json);
 
-					return json_encode(array('status' => 'sucesso', 'dados' => $retorno));
-				} else {
-					return json_encode(array('status' => 'erro', 'dados' => 'Método inexistente!'));
-				}
-			} else {
-				return json_encode(array('status' => 'erro', 'dados' => 'Classe inexistente!'));
-			}	
-		} catch (Exception $e) {
-			return json_encode(array('status' => 'erro', 'dados' => $e->getMessage()));
-		}
-		
-	}
+            $erro = call_user_func_array(array(new $classe, 'validaDado'), array($post));
+            if (!$erro) {
+                $retorno = call_user_func_array(array(new $classe, 'insert'), array($post));
+            }
+
+        } elseif ($tipo_requisicao == 'PUT') {
+            $json = file_get_contents('php://input');
+            $post = json_decode($json);
+
+            if ($parametros[0] > 0) {
+                $id = $parametros[0];
+
+                $erro = call_user_func_array(array(new $classe, 'validaDado'), array($post));
+                if (!$erro) {
+                    $registro = call_user_func_array(array(new $classe, 'find'), $parametros);
+                    if (empty($registro)) {
+                        $erro['status'] = 'erro';
+                        $erro['dados'] = "Registro $id do objeto $classe não localizado";
+                    } else {
+
+                        $retorno = call_user_func_array(array(new $classe, 'update'), array($post, $id));
+
+                    }
+                }
+            }
+        } elseif ($tipo_requisicao == 'DELETE') {
+
+            @$id = $parametros[0];
+            if ($id > 0) {
+                $registro = call_user_func_array(array(new $classe, 'find'), $parametros);
+                if (empty($registro)) {
+                    $erro['status'] = 'erro';
+                    $erro['dados'] = "Registro $id do objeto $classe não localizado";
+                } else {
+                    $retorno = call_user_func_array(array(new $classe, 'delete'), array($id));
+
+                }
+            }
+        }
+        if ($erro) {
+            return json_encode($erro);
+        } else {
+            return json_encode(array('status' => 'sucesso', 'dados' => $retorno));
+        }
+    }
 }
 
 if (isset($_REQUEST)) {
-	echo Rest::open($_REQUEST);
+    echo Rest::open($_REQUEST);
 }
